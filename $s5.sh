e0b2389e2566fb1d5280bd403e26d21745488b2c
XavierAtc94 <jogongorahi@uide.edu.ec>
2026-04-02 01:58:24 +0000

    Wip my first script

---
+++ $s5.sh
@@ -0,0 +1,5 @@
+#lista de archivos y directorios
+#comando mkdir crea directorios (estos se resaltan)
+#comando ls lista archivos y directorios
+#comando touch crea archivos (no se resaltan)
+#comando cd cambia directorios (con . sube un directorio)
