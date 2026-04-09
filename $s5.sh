d6ded9c7aba6fa17b46f0fdfc0a1e0982f129b7f
XavierAtc94 <jogongorahi@uide.edu.ec>
2026-04-09 01:43:11 +0000

    mfpwsh script

--- $s5.sh
+++ $s5.sh
@@ -3,3 +3,7 @@
 #comando ls lista archivos y directorios
 #comando touch crea archivos (no se resaltan)
 #comando cd cambia directorios (con . sube un directorio)
+#comando rm quita paquetes o repositorios
+#sudo dpkg -i instala 
+#sudo apt-get install -y (aqui lo que queremos instalar)
+#powershell se entra con el cmd pwsh
---
+++ pwsh_script.ps1
@@ -0,0 +1,6 @@
+$a=2 #con a se crean variables de numero
+$b="chao" # con b creas variables de string
+Write-Host $a #imprime
+write-output $b #imprime
+Write-Host "Hola" -ForegroundColor Green -BackgroundColor magenta #se elige el color de letra o fondo etc
+#string para cadenas, int para enteros y array para listas (va con @ y entre parentesis)
